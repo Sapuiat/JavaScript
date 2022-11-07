191d3253e1555be90ec786b44ce37d66a971d1f6
Miguel Nunes <miguelpay157@gmail.com>
2022-11-07 13:46:32 -0300

    Update

--- ex001/php.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$server_seed = "c8197a717225a95d9bb7a797d4db1f6ff73950e0a59bb2d690a8acf715b331b4";
-$public_seed = "a2e31a2afceff13467729237b1c2aa1c";
-$round = "644954";
-$hash = hash('sha256', $server_seed . "-" . $public_seed . "-" . $round);
-$roll = hexdec(substr($hash, 0, 8)) % 15;
-
-echo("Roll: $roll");
